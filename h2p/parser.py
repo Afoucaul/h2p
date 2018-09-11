b344b256fb49d10851afc13555e21683effc7e71
armand <armand.foucault@telecom-bretagne.eu>
2018-09-11 18:10:20 +0200

    Wrote base for list comprehension parsing

--- h2p/parser.py
+++ h2p/parser.py
@@ -17,14 +17,23 @@ class Expression(AST): pass
 class Number(AST) : pass
 class Application(AST): pass
 class Expression(AST): pass
+class Pattern(AST): pass
 class Lambda(AST): pass
 class Value(AST): pass
 class Number(AST): pass
-class EmptyList(AST): pass
-class ListComprehension(AST): pass
-class ListEnumeration(AST): pass
+class List(AST): pass
+class EmptyList(List): pass
+class ListEnumeration(List): pass
 
-class Range(AST):
+
+class ListComprehension(List):
+    def __init__(self, element, productions: [(str, List)], conditions):
+        super().__init__(element, productions, conditions)
+        self.element = element
+        self.productions = productions
+        self.confitions = conditions
+
+class Range(List):
     def __init__(self, first, last, second):
         super().__init__(first, last, second)
         self.first = first
@@ -68,7 +77,7 @@ def p_expressions(p):
 
 def p_infix_operator(p):
     '''infix_operator : PLUS 
-                      | MINUS 
+                      | DASH 
                       | SLASH
                       | STAR'''
     p[0] = p[1]
@@ -102,6 +111,7 @@ def p_list(p):
 
 def p_list_description(p):
     '''list_description : list_enumeration
+                        | list_comprehension
                         | range'''
     p[0] = p[1]
 
@@ -109,6 +119,48 @@ def p_list_description(p):
 def p_list_enumeration(p):
     '''list_enumeration : list_values'''
     p[0] = ListEnumeration(p[1])
+
+
+def p_list_comprehension(p):
+    '''list_comprehension : application BAR comprehension_productions
+                          | application BAR comprehension_productions COMMA comprehension_conditions
+                          '''
+    if len(p) == 4:
+        p[0] = ListComprehension(p[1], p[3], None)
+    elif len(p) == 6:
+        p[0] = ListComprehension(p[1], p[3], p[5])
+
+def p_comprehension_productions(p):
+    '''comprehension_productions : comprehension_production
+                                 | comprehension_production COMMA comprehension_productions'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    elif len(p) == 3:
+        p[0] = [p[1]] + p[2]
+
+
+def p_comprehension_production(p):
+    '''comprehension_production : pattern LT DASH list'''
+    p[0] = (p[1], p[4])
+
+
+def p_comprehension_conditions(p):
+    '''comprehension_conditions : comprehension_condition
+                                 | comprehension_condition COMMA comprehension_conditions'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    elif len(p) == 3:
+        p[0] = [p[1]] + p[2]
+
+
+def p_comprehension_condition(p):
+    '''comprehension_condition : pattern LT DASH list'''
+    p[0] = (p[1], p[4])
+    
+
+def p_pattern(p):
+    '''pattern : IDENT'''
+    p[0] = Pattern(p[1])
 
 
 def p_list_values(p):
