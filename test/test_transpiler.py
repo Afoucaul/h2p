f372da05de76cac1bdaad942a20651407fe2c87f
armand <armand.foucault@telecom-bretagne.eu>
2018-09-17 01:11:24 +0200

    Added several tests for range transpiling

--- test/test_transpiler.py
+++ test/test_transpiler.py
@@ -23,7 +23,11 @@ from h2p.utils import compare_ast
 _print = print
 def myprint(x, *args, **kwargs):
     if isinstance(x, ast.AST):
-        astpretty.pprint(x, *args, **kwargs)
+        try:
+            astpretty.pprint(x, *args, **kwargs)
+        except:
+            print("Failed pretty printing AST")
+            _print(x, *args, **kwargs)
     else:
         _print(x, *args, **kwargs)
 print = myprint
@@ -47,6 +51,40 @@ class TestH2PTranspiler(unittest.TestCase):
     def test_simple_enumerated_list(self):
         inputData = "[1, 2, 3]"
         expected = ast.Expr(ast.List([ast.Num(1), ast.Num(2), ast.Num(3)], None))
+        result = transpile(inputData)
+
+        self.assertTrue(compare_ast(result, expected))
+
+    def test_range_with_first_and_last(self):
+        inputData = "[1..10]"
+        expected = ast.Expr(ast.Call(ast.Name("range", None), [ast.Num(1), ast.Num(11)], None))
+        result = transpile(inputData)
+
+        self.assertTrue(compare_ast(result, expected))
+
+    def test_range_with_first_second_and_last(self):
+        inputData = "[1, 3..10]"
+        expected = ast.Expr(ast.Call(
+            ast.Name("range", None), 
+            [ast.Num(1), ast.Num(11), ast.Num(2)], None))
+        result = transpile(inputData)
+
+        self.assertTrue(compare_ast(result, expected))
+
+    def test_infinite_range(self):
+        inputData = "[1..]"
+        expected = ast.Expr(ast.Call(
+            ast.Attribute(ast.Name("itertools", None), "count", None),
+            [], [ast.keyword("start", ast.Num(1)), ast.keyword("step", ast.Num(1))]))
+        result = transpile(inputData)
+
+        self.assertTrue(compare_ast(result, expected))
+
+    def test_infinite_range_with_step(self):
+        inputData = "[1, 3..]"
+        expected = ast.Expr(ast.Call(
+            ast.Attribute(ast.Name("itertools", None), "count", None),
+            [], [ast.keyword("start", ast.Num(1)), ast.keyword("step", ast.Num(2))]))
         result = transpile(inputData)
 
         self.assertTrue(compare_ast(result, expected))
