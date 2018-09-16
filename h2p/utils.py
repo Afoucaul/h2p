b7d8f5ceaff91902947dd208df321491508060fd
armand <armand.foucault@telecom-bretagne.eu>
2018-09-17 00:12:09 +0200

    Fixed ast comparison function

--- h2p/utils.py
+++ h2p/utils.py
@@ -4,11 +4,16 @@ def compare_ast(t1, t2):
     if type(t1) != type(t2):
         return False
 
+    if isinstance(t1, list):
+        return len(t1) == len(t2) and all(compare_ast(c1, c2) for c1, c2 in zip(t1, t2))
+
+    if not isinstance(t1, ast.AST):
+        return t1 == t2
+
     for k, c1 in t1.__dict__.items():
         c2 = t2.__dict__[k]
-        equals = ((isinstance(c1, ast.AST) and type(c1) == type(c2) and compare_ast(c1, c2))
-                or c1 == c2)
-        if not equals:
+        if not compare_ast(c1, c2):
+            print("Attribute {} differs:".format(k), c1, c2)
             return False
 
     return True
